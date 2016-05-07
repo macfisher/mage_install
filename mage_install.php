c7d04e952ef3b5fa16404b77822e5b943ace3af1
macfisher <macfisher01@gmail.com>
2016-05-07 17:32:49 +0000

    initial commit

---
+++ mage_install.php
@@ -0,0 +1,29 @@
+<?php
+
+// clone Magento 1.7 mirror
+system("sudo -u root git clone https://github.com/amacgregor/magento-1.7.0.2.git install");
+
+// set permissions
+system("sudo -u root chown -R www-data:www-data install/");
+system("sudo -u root chmod -R 755 install/var/");
+system("sudo -u root chmod -R 755 install/media/");
+system("sudo -u root chmod -R 755 install/app/etc/");
+
+$servername = "localhost";
+$username = "root";
+$password = "root";
+
+// Create connection to MySQL
+$conn = new mysqli($servername, $username, $password);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+// Create database
+$sql = "CREATE DATABASE magento";
+if ($conn->query($sql) === TRUE) {
+    echo "\nDatabase created successfully";
+} else {
+    echo "\nError creating database: " . $conn->error;
+}
